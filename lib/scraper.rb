789786d24dd9a2240231075f070f78bf6b849e68
Delia Constantino <dconstan92@gmail.com>
2020-11-17 18:34:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,7 @@ class Scraper
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     # binding.pry
-    puts .css(".post")
+    puts doc.css(".post")
 
   end
 
